a9aa369e929ae7b16998ff28b73b75377053299d
Jeff Sonderman <jeff.sonderman@pressinstitute.org>
2026-02-16 00:25:50 -0500

    Fix PHP deprecation notice in ET_Client __doRequest()

--- src/ET_Client.php
+++ src/ET_Client.php
@@ -540,7 +540,13 @@ class ET_Client extends SoapClient
 	 * @param integer $one_way Future use
 	 * @return string Soap web service request result
 	 */			
-	function __doRequest($request, $location, $saction, $version, $one_way = 0) 
+	public function __doRequest(
+		string $request,
+		string $location,
+		string $action,
+		int $version,
+		bool $oneWay = false
+	): ?string 
 	{
 		$doc = new DOMDocument();
 		$doc->loadXML($request);
